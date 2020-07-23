35fd144b88785ed9b2b32eac9944c6d85211cd60
Charlie-Mei <charlie.mei@outlook.com>
2020-07-22 23:33:02 -0400

    creating venv for nlp work

---
+++ assignments/A7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'nlp_toolkit',
+    packages = find_packages(),
+    version = '0.0.1',
+    author = 'Charlie Mei',
+    description = 'Collection of functions learnt from the NLP analytics class'
+)
